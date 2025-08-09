1d4ba91320c0b7485c0e1a79ba08bf944855a8e0
Hyziri <hyziri@protonmail.com>
2025-08-09 04:04:01 -0500

    Add documentation to oauth2 models

--- src/model/oauth2.rs
+++ src/model/oauth2.rs
@@ -1,7 +1,13 @@
 use serde::{Deserialize, Serialize};
 
+/// Represents the data needed to begin an OAuth2 authentication flow
+///
+/// This struct contains the URL where users should be redirected to login
+/// and a random state parameter for CSRF protection.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct AuthenticationData {
+    /// The URL where users should be redirected to login
     pub login_url: String,
+    /// A random state parameter used to prevent CSRF attacks
     pub state: String,
 }
