2bb20668f48aa395617cc8699e3b8d79729a481c
Hyziri <hyziri@protonmail.com>
2025-08-11 21:38:16 -0500

    Add module documentation for the oauth2 module.

--- src/oauth2/mod.rs
+++ src/oauth2/mod.rs
@@ -1,3 +1,29 @@
+//! Methods for OAuth2 authentication with EVE Online SSO
+//!
+//! This module provides methods for initiating and managing the EVE Online OAuth2 authentication process
+//! It includes functionality for generating login URLs to initiate the authentication process and building scopes for authorization.
+//!
+//! # References
+//! - [EVE SSO Documentation](https://developers.eveonline.com/docs/services/sso/)
+//!
+//! # Example
+//! ```
+//! let esi_client = eve_esi::Client::new()
+//!     .user_agent("MyApp/1.0 (contact@example.com)")
+//!     .client_id("client_id")
+//!     .client_secret("client_secret")
+//!     .callback_url("http://localhost:8080/callback");
+//!
+//! let scopes = eve_esi::oauth2::ScopeBuilder::new()
+//!     .public_data()
+//!     .build();
+//! let auth_data = esi_client
+//!     .initiate_oauth_login(scopes)
+//!     .unwrap();
+//!
+//! println!("Login URL: {}", auth_data.login_url);
+//! ```
+
 pub mod auth;
 pub mod scope;
 
