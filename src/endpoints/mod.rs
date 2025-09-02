e5f9bba45e3e3e549ac128d28902608ced47cdf8
Hyziri <hyziri@protonmail.com>
2025-09-02 05:23:35 -0500

    Refactor: Move ESI endpoint impls for EsiClient into endpoints.rs

--- src/endpoints/mod.rs
+++ src/endpoints/mod.rs
@@ -26,31 +26,7 @@
 //! }
 //! ```
 
-use crate::EsiClient;
-
 pub mod alliance;
 pub mod character;
 pub mod corporation;
-
-impl EsiClient {
-    /// Access to Alliance ESI endpoints
-    ///
-    /// Returns an API client for interacting with alliance-related endpoints.
-    pub fn alliance(&self) -> self::alliance::AllianceApi<'_> {
-        self::alliance::AllianceApi::new(self)
-    }
-
-    /// Access to Character ESI endpoints
-    ///
-    /// Returns an API client for interacting with character-related endpoints.
-    pub fn character(&self) -> self::character::CharacterApi<'_> {
-        self::character::CharacterApi::new(self)
-    }
-
-    /// Access to Corporation ESI endpoints
-    ///
-    /// Returns an API client for interacting with corporation-related endpoints.
-    pub fn corporation(&self) -> self::corporation::CorporationApi<'_> {
-        self::corporation::CorporationApi::new(self)
-    }
-}
+pub mod endpoints;
