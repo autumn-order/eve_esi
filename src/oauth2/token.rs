f41a89e47f7fba019294000e92256dabd27a3132
Hyziri <hyziri@protonmail.com>
2025-09-08 21:36:28 -0500

    Feat: Add retry attempt when validating refresh token.

--- src/oauth2/token.rs
+++ src/oauth2/token.rs
@@ -63,6 +63,7 @@ use oauth2::{AuthorizationCode, EmptyExtraTokenFields, StandardTokenResponse};
 use crate::error::{Error, OAuthError};
 use crate::model::oauth2::{EveJwtClaims, EveJwtKey, EveJwtKeys};
 use crate::oauth2::OAuth2Api;
+use crate::Client;
 
 impl<'a> OAuth2Api<'a> {
     /// Retrieves a token from EVE Online's OAuth2 API
@@ -109,7 +110,12 @@ impl<'a> OAuth2Api<'a> {
     /// fetched EVE's OAuth2 API or retrieved from cache via the
     /// [`crate::oauth2::jwk::JwkApi::get_jwt_keys`] method.
     ///
-    /// For an overview & usage, see the [module-level documentation](super)
+    /// This function will make 2 attempts to validate a token, if the first attempt
+    /// fails the JWT key cache will be cleared and a refresh attempt will be made.
+    /// This is useful for when EVE Online rotates the JWT keys used to validate
+    /// tokens and the keys need to be refetched.
+    ///
+    /// For a general overview on tokens & usage, see the [module-level documentation](super)
     ///
     /// # Documentation
     /// See <https://developers.eveonline.com/docs/services/sso/#validating-jwt-tokens>
@@ -119,63 +125,91 @@ impl<'a> OAuth2Api<'a> {
     ///   `token.access_token().secret().to_string()` on the token returned from [`Self::get_token`].
     ///
     /// # Errors
-    /// - [`Error`]: If there is an issue retrieving JWT keys from ESI Client's cache or there is an
-    ///   issue validating the token.
+    /// - [`Error`]: If the retry attempt fails and there is an issue retrieving JWT keys from ESI Client's
+    ///   cache or there is an issue validating the token.
     pub async fn validate_token(&self, token_secret: String) -> Result<EveJwtClaims, Error> {
-        // Get JWT keys to validate token
-        #[cfg_attr(coverage_nightly, coverage(off))]
-        debug!("Retrieving keys for validation from JWT key cache");
+        // First attempt
+        match attempt_validation(&self.client, &token_secret).await {
+            Ok(claims) => Ok(claims),
+            Err(err) => {
+                // Log the attempt
+                #[cfg_attr(coverage_nightly, coverage(off))]
+                debug!(
+                    "First attempt to validate token failed, retrying token validation: {:?}",
+                    &err
+                );
 
-        let jwt_keys = self.jwk().get_jwt_keys().await?;
+                // Clear the cache to trigger a JWT key refresh on next attempt
+                self.client.inner.jwt_key_cache.clear_cache().await;
 
-        // Configure validation
-        let mut validation = Validation::new(jsonwebtoken::Algorithm::RS256);
-        validation.set_audience(&[self.client.inner.jwt_audience.to_string()]);
-        validation.set_issuer(&[self.client.inner.jwt_issuer.to_string()]);
-
-        // Try to find an RS256 key
-        #[cfg_attr(coverage_nightly, coverage(off))]
-        debug!("Checking JWT key cache for RS256 key");
-
-        if let Some(EveJwtKey::RS256 { ref n, ref e, .. }) = get_first_rs256_key(&jwt_keys) {
-            // RS256 key was found, extract n (modulus) and e (exponent) components for the decoding key
-            #[cfg_attr(coverage_nightly, coverage(off))]
-            debug!("Creating a decoding key from RS256 key");
-
-            let decoding_key = match DecodingKey::from_rsa_components(n, e) {
-                Ok(key) => key,
-                Err(err) => {
-                    #[cfg_attr(coverage_nightly, coverage(off))]
-                    error!("Failed to decode RS256 key for token validation: {}", &err);
-
-                    return Err(Error::OAuthError(OAuthError::ValidateTokenError(err)));
-                }
-            };
-
-            // Validate the token
-            #[cfg_attr(coverage_nightly, coverage(off))]
-            debug!("Validating token using RS256 decoding key");
-
-            match jsonwebtoken::decode::<EveJwtClaims>(&token_secret, &decoding_key, &validation) {
-                Ok(token_data) => Ok(token_data.claims),
-                Err(err) => {
-                    #[cfg_attr(coverage_nightly, coverage(off))]
-                    error!("Failed to validate token with RS256 key: {}", &err);
-
-                    Err(Error::OAuthError(OAuthError::ValidateTokenError(err)))
-                }
+                // Second attempt (retry)
+                attempt_validation(&self.client, &token_secret).await
             }
-        } else {
-            // No RS256 key was found
-            let message: &str =  "Failed to find RS256 key in JWT key cache when attempting to validate a JWT token.";
-
-            #[cfg_attr(coverage_nightly, coverage(off))]
-            error!("{}", message);
-
-            Err(Error::OAuthError(OAuthError::NoValidKeyFound(
-                message.to_string(),
-            )))
         }
+    }
+}
+
+/// Attempts to validate a token retrieved via the [`Self::get_token`] method
+///
+/// This is the internal utility method for token validation, see [`OAuth2Api::validate_token`]
+/// for an overview.
+///
+/// # Arguments
+/// - `client` (&[`Client`]): client used to make ESI & EVE OAuth2 requests and cache JWT keys
+/// - `token_secret` ([`String`]): The access token secret as a string. You can use
+///   `token.access_token().secret().to_string()` on the token returned from [`Self::get_token`].
+///
+/// # Errors
+/// - [`Error`]: If there is an issue retrieving JWT keys from ESI Client's cache or there is an
+///   issue validating the token.
+async fn attempt_validation(client: &Client, token_secret: &str) -> Result<EveJwtClaims, Error> {
+    // Get JWT keys to validate token
+    debug!("Retrieving keys for validation from JWT key cache");
+
+    let jwt_keys = client.oauth2().jwk().get_jwt_keys().await?;
+
+    // Configure validation
+    let mut validation = Validation::new(jsonwebtoken::Algorithm::RS256);
+    validation.set_audience(&[client.inner.jwt_audience.to_string()]);
+    validation.set_issuer(&[client.inner.jwt_issuer.to_string()]);
+
+    // Try to find an RS256 key
+    debug!("Checking JWT key cache for RS256 key");
+
+    if let Some(EveJwtKey::RS256 { ref n, ref e, .. }) = get_first_rs256_key(&jwt_keys) {
+        // RS256 key was found, extract n (modulus) and e (exponent) components for the decoding key
+        debug!("Creating a decoding key from RS256 key");
+
+        let decoding_key = match DecodingKey::from_rsa_components(n, e) {
+            Ok(key) => key,
+            Err(err) => {
+                error!("Failed to decode RS256 key for token validation: {}", &err);
+
+                return Err(Error::OAuthError(OAuthError::ValidateTokenError(err)));
+            }
+        };
+
+        // Validate the token
+        debug!("Validating token using RS256 decoding key");
+
+        match jsonwebtoken::decode::<EveJwtClaims>(&token_secret, &decoding_key, &validation) {
+            Ok(token_data) => Ok(token_data.claims),
+            Err(err) => {
+                error!("Failed to validate token with RS256 key: {}", &err);
+
+                Err(Error::OAuthError(OAuthError::ValidateTokenError(err)))
+            }
+        }
+    } else {
+        // No RS256 key was found
+        let message: &str =
+            "Failed to find RS256 key in JWT key cache when attempting to validate a JWT token.";
+
+        error!("{}", message);
+
+        Err(Error::OAuthError(OAuthError::NoValidKeyFound(
+            message.to_string(),
+        )))
     }
 }
 
