ce120a49467ea3977d00b3e7191f40ae6d292de2
Hyziri <hyziri@protonmail.com>
2025-09-13 00:27:01 -0500

    Feat: add mock() method to EveJwtClaims to create mock claims.

--- src/model/oauth2.rs
+++ src/model/oauth2.rs
@@ -154,6 +154,39 @@ pub struct EveJwtClaims {
     pub name: String,
     /// The character's ID
     pub owner: String,
-    /// The character's organization (corporation/alliance) ID
+    /// Client ID
     pub azp: String,
+}
+
+impl EveJwtClaims {
+    /// Utility function to create a mock of EveJwtClaims
+    pub fn mock() -> Self {
+        // Get current unix timestamp
+        let unix_timstamp_now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs() as i64;
+
+        // Create JWT mock claims matching what EVE Online would return
+        EveJwtClaims {
+            // ESI SSO docs defines 2 different JWT issuers but typically only returns 1 of them at a time
+            // The default defines 2 but for tests we'll define 1 to ensure validation works
+            iss: "https://login.eveonline.com".to_string(),
+            sub: "CHARACTER:EVE:123456789".to_string(),
+            aud: vec!["client_id".to_string(), "EVE Online".to_string()],
+            jti: "abc123def456".to_string(),
+            kid: "JWT-Signature-Key-1".to_string(),
+            tenant: "tranquility".to_string(),
+            region: "world".to_string(),
+            exp: unix_timstamp_now + 900, // Valid for 15 minutes
+            iat: unix_timstamp_now,
+            scp: vec![
+                "publicData".to_string(),
+                "esi-characters.read_agents_research.v1".to_string(),
+            ],
+            name: "Test Character".to_string(),
+            owner: "123456789".to_string(),
+            azp: "client_id".to_string(),
+        }
+    }
 }
