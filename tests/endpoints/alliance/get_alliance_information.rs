9d19fe42edad33a683de41aa1247fe8421608859
Hyziri <hyziri@protonmail.com>
2025-09-15 06:35:09 -0500

    Tests: refactor alliance endpoint integration tests to deserialize JSON.

--- tests/endpoints/alliance/get_alliance_information.rs
+++ tests/endpoints/alliance/get_alliance_information.rs
@@ -1,95 +1,60 @@
-use eve_esi::model::alliance::Alliance;
-
 use crate::util::setup;
 
-/// Tests the successful retrieval of alliance information from a mock EVE ESI server.
-///
-/// # Test Setup
-/// - Setup a basic EsiClient & mock HTTP server
-/// - Create mock alliance data
-/// - Configure mock server with an ESI endpoint returning the mock alliance
-///
-/// # Assertions
-/// - Assert 1 request was made to the mock server
-/// - Assert result is Ok
-/// - Assert received expected alliance data
+/// Successful retrieval of alliance information
 #[tokio::test]
 async fn get_alliance_information() {
-    // Setup a basic EsiClient & mock HTTP server
     let (esi_client, mut mock_server) = setup().await;
 
-    // Create mock alliance data
-    let mock_alliance = Alliance {
-        creator_corporation_id: 98784257,
-        creator_id: 2114794365,
-        faction_id: None,
-        date_founded: "2024-09-25T06:25:58Z".parse().unwrap(),
-        executor_corporation_id: Some(98787881),
-        name: "Autumn.".to_string(),
-        ticker: "AUTMN".to_string(),
-    };
+    let mock_alliance = serde_json::json!({
+        "creator_corporation_id": 98784257,
+        "creator_id": 2114794365,
+        "faction_id": null,
+        "date_founded": "2024-09-25T06:25:58Z",
+        "executor_corporation_id": 98787881,
+        "name": "Autumn.",
+        "ticker": "AUTMN",
+    });
 
-    // Configure mock server with an ESI endpoint returning the mock alliance
-    let mock = mock_server
+    let mock_alliance_endpoint = mock_server
         .mock("GET", "/alliances/99013534/")
         .with_status(200)
         .with_header("content-type", "application/json")
-        .with_body(serde_json::to_string(&mock_alliance).unwrap())
+        .with_body(mock_alliance.to_string())
         .create();
 
-    // Retrieve the alliance
     let result = esi_client
         .alliance()
         .get_alliance_information(99013534)
         .await;
 
-    // Assert 1 request was made to the mock server
-    mock.assert();
+    // Assert 1 request was made to the mock endpoint
+    mock_alliance_endpoint.assert();
 
-    // Assert result is Ok
     assert!(result.is_ok());
-
-    // Assert received expected alliance data
-    let alliance = result.unwrap();
-    assert_eq!(alliance, mock_alliance);
 }
 
-/// Tests receiving a 404 error when attempting to retrieve alliance information from a mock EVE ESI server.
-///
-/// # Test Setup
-/// - Setup a basic EsiClient & mock HTTP server
-/// - Configure mock server with an ESI endpoint returning 404 not found
-///
-/// # Assertions
-/// - Assert 1 request was made to the mock server
-/// - Assert result is error
-/// - Assert error is due to internal server error
+/// Receiving a 404 error when attempting to retrieve alliance information
 #[tokio::test]
 async fn get_alliance_information_not_found() {
-    // Setup a basic EsiClient & mock HTTP server
     let (esi_client, mut mock_server) = setup().await;
 
-    // Configure a mock response returning a 404 not found
-    let mock = mock_server
+    let mock_alliance_endpoint = mock_server
         .mock("GET", "/alliances/99999999/")
         .with_status(404)
         .with_header("content-type", "application/json")
         .with_body(r#"{"error": "Alliance not found"}"#)
         .create();
 
-    // Retrieve the alliance
     let result = esi_client
         .alliance()
         .get_alliance_information(99999999)
         .await;
 
-    // Assert 1 request was made to the mock server
-    mock.assert();
+    // Assert 1 request was made to the mock endpoint
+    mock_alliance_endpoint.assert();
 
-    // Assert result is error
     assert!(result.is_err());
 
-    // Assert error is due to internal server error
     assert!(
         matches!(result, Err(eve_esi::Error::ReqwestError(ref e)) if e.status() == Some(reqwest::StatusCode::NOT_FOUND))
     );
--- tests/endpoints/alliance/list_all_alliances.rs
+++ tests/endpoints/alliance/list_all_alliances.rs
@@ -1,57 +1,32 @@
 use crate::util::setup;
 
 /// Successful retrieval of list of alliance IDs
-///
-/// # Test Setup
-/// - Setup a basic EsiClient & mock HTTP server
-/// - Create mock Vec of alliance IDs
-/// - Configure mock server with endpoint returning mock alliance IDs
-///
-/// # Assertions
-/// - Assert 1 request was made to the mock server
-/// - Assert result is Ok
 #[tokio::test]
 async fn test_list_all_alliances_success() {
-    // Setup a basic EsiClient & mock HTTP server
     let (esi_client, mut mock_server) = setup().await;
 
-    // Create mock Vec of alliance IDs
-    let mock_alliance_ids = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let mock_alliance_ids = serde_json::json!([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
-    // Configure mock server with endpoint returning mock alliance IDs
-    let mock = mock_server
+    let mock_alliance_endpoint = mock_server
         .mock("GET", "/alliances")
         .with_status(200)
         .with_header("content-type", "application/json")
-        .with_body(serde_json::to_string(&mock_alliance_ids).unwrap())
+        .with_body(mock_alliance_ids.to_string())
         .create();
 
-    // Retrieve the list of alliances
     let result = esi_client.alliance().list_all_alliances().await;
 
-    // Assert 1 request was made to the mock server
-    mock.assert();
+    // Assert 1 request was made to the mock endpoint
+    mock_alliance_endpoint.assert();
 
-    // Assert result is Ok
     assert!(result.is_ok());
 }
 
 /// Receiving an error 500 when calling list_all_alliances()
-///
-/// # Test Setup
-/// - Setup a basic EsiClient & mock HTTP server
-/// - Configure mock server with an ESI endpoint returning a 600 internal server error
-///
-/// # Assertions
-/// - Assert 1 request was made to the mock server
-/// - Assert result is error
-/// - Assert error is due to internal server error
 #[tokio::test]
 async fn test_list_all_alliances_internal_error() {
-    // Setup a basic EsiClient & mock HTTP server
     let (esi_client, mut mock_server) = setup().await;
 
-    // Configure a mock response returning a 500 internal server error
     let mock = mock_server
         .mock("GET", "/alliances")
         .with_status(500)
@@ -59,16 +34,13 @@ async fn test_list_all_alliances_internal_error() {
         .with_body(r#"{"error": "Internal server error"}"#)
         .create();
 
-    // Retrieve the list of alliances
     let result = esi_client.alliance().list_all_alliances().await;
 
-    // Assert 1 request was made to the mock server
+    // Assert 1 request was made to the mock endpoint
     mock.assert();
 
-    // Assert result is error
     assert!(result.is_err());
 
-    // Assert error is due to internal server error
     assert!(
         matches!(result, Err(eve_esi::Error::ReqwestError(ref e)) if e.status() == Some(reqwest::StatusCode::INTERNAL_SERVER_ERROR))
     );
